8d72c2965999a42e180ac25b5abcc5594367cda8
Wasim <wasimekram123@gmail.com>
2020-02-12 19:14:53 +0530

    fixed by removing mongodb

--- app.js
+++ app.js
@@ -17,19 +17,6 @@ app.set('view engine', 'hbs');
 app.set('views', viewsPath);
 hbs.registerPartials(partialsPath);
 
-const {MongoClient, ObjectID} = require('mongodb');
-
-const connectionURL = 'mongodb://127.0.0.1:27017';
-const databaseName = 'taskmanager';
-
-MongoClient.connect(connectionURL, {useNewUrlParser: true}, (error, client) => {
-    if (error) {
-        return console.log('Unable to connect to database!')
-    }
-    const db = client.db(databaseName)
-
-});
-
 app.get('/about', (req, res) => {
     res.render('about', {
         title: "About page",
